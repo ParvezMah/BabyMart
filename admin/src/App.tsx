d6229af50af078ce182084bff64d2f9de8598930
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2026-05-10 15:09:53 +0600

    part-2 | 03:45:00 | Admin's Brands Management Completed

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -2,13 +2,12 @@ import { Navigate, Outlet } from "react-router";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import { Toaster } from "./components/ui/sonner";
-import { cn } from "./lib/utils";
 import useAuthStore from "./store/useAuthStore";
 import { useState } from "react";
-
+import { cn } from "./lib/utils";
 
 function App() {
-  
+
   const { isAuthenticated } = useAuthStore();
   const [sidebarOpen, setSidebarOpen] = useState(true);
   
@@ -19,19 +18,17 @@ function App() {
   return (
     <div className="h-screen flex bg-background">
       <Sidebar open={sidebarOpen} setOpen={setSidebarOpen}/>
-        <div className={cn(
-            "flex flex-col flex-1 max-w-[--breakpoint-2xl]",
-            sidebarOpen ? "md:ml-64" : "md:ml-20"
-          )}
-        >
-        <div className="flex flex-col flex-1 max-w-[--breakpoint-2xl] ml-64">
+      <div className={cn(
+          "flex flex-col flex-1 max-w-[--breakpoint-2xl]",
+          sidebarOpen ? "md:ml-64" : "md:ml-20"
+        )}
+>
         <Header/>
         <main>
           <Outlet/>
         </main>
-        </div>
-        <Toaster position="bottom-right"/>
       </div>
+      <Toaster position="bottom-right"/>
     </div>
   );
 }
