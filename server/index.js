2b1d3a1143b8011ed9270df2b37e5ff229823e18
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2026-05-10 10:05:46 +0600

    part-1 | 02:45:00 | categoryRoutes & categoryController api added

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js"
 import productRoutes from "./routes/productRoutes.js"
 import brandRoutes from "./routes/brandRoutes.js"
+import categoryRoutes from "./routes/categoryRoutes.js"
 
 import swaggerUi from "swagger-ui-express";
 import { specs } from "./config/swagger.js";
@@ -71,6 +72,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/brands", brandRoutes);
+app.use("/api/categories", categoryRoutes);
 
 // API Documentation
 app.use(
