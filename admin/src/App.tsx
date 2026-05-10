0ef6f4f23a2c7046f81f3c52120421455431fd4e
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2026-05-10 14:29:01 +0600

    part-2 | 00:39:00 | Header design setup

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -8,13 +8,13 @@ function App() {
   return (
     <div className="h-screen flex bg-background">
       <Sidebar/>
-      <div className="flex flex-col flex-1 max-w-[--breakpoint-2xl] ml-64 ">
+      <div className="flex flex-col flex-1 max-w-[--breakpoint-2xl] ml-64">
         <Header/>
         <main>
           <Outlet/>
         </main>
       </div>
-      <Toaster/>
+      <Toaster position="bottom-right"/>
     </div>
   );
 }
