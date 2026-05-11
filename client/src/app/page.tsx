e9de48b992607a71fb41ad23819e893841d00b14
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2026-05-11 17:28:09 +0600

    part-3 | 02:30:00 | Setup HomeBrand Section

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,10 +1,13 @@
 import Container from "@/components/common/Container";
 import Banner from "@/components/home/Banner";
 import CategoriesSection from "@/components/home/CategoriesSection";
+import HomeBrand from "@/components/home/HomeBrand";
 import ProductList from "@/components/home/ProductList";
+import { Brand } from "../../type";
+import { fetchData } from "@/lib/api";
 
 export default async function Home() {
-
+  const brands = await fetchData<Brand[]>("/brands");
   return (
     <div>
       <Container className="min-h-screen flex py-7 gap-3">
@@ -12,8 +15,8 @@ export default async function Home() {
         <div className="flex-1">
           <Banner />
           <ProductList />
-          {/* <HomeBrand brands={brands} />
-          <BabyTravelSection />
+          <HomeBrand brands={brands} />
+        {/* <BabyTravelSection />
           <ComfyApparelSection />
           <FeaturedServicesSection /> */}
         </div>
