fec343e1476ac0bb1e29de6853d6d55012b376c2
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2026-05-12 15:21:14 +0600

    part-4 | 01:22:00 | zustand store setup

---
+++ client/src/components/common/pages/product/WishlistButton.tsx
@@ -0,0 +1,26 @@
+import { cn } from "@/lib/utils";
+import { Heart } from "lucide-react";
+import React from "react";
+
+interface Props {
+  className?: string;
+}
+
+const WishlistButton = ({ className }: Props) => {
+  return (
+    <button
+      className={cn(
+        "p-2 rounded-full transition-colors hover:bg-gray-100",
+        className
+      )}
+      title="wishlist-btn"
+    >
+      <Heart
+        size={20}
+        // className={isLoading ? "animate-pulse" : ""}
+      />
+    </button>
+  );
+};
+
+export default WishlistButton;
