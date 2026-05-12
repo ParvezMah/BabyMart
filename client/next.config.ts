c5ee8bb28580f4c99d61015ce60c8a52e7559707
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2026-05-12 16:06:48 +0600

    part-4 | 01:53:00 | Sign In & Form Setup

--- client/next.config.ts
+++ client/next.config.ts
@@ -7,6 +7,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       { protocol: "https", hostname: "res.cloudinary.com" },
       { protocol: "https", hostname: "d2v5dzhdg4zhx3.cloudfront.net" },
+      { protocol: "https", hostname: "img.freepik.com" },
 
     ],
     },
